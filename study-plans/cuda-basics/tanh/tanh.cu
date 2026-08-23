e4347b89615e7ae6d7f392de1051662e0bcc557e
Raghav K <ragg04@gmail.com>
2026-08-22 21:08:18 -0700

    Solve tanh

---
+++ study-plans/cuda-basics/tanh/tanh.cu
@@ -0,0 +1,21 @@
+#include <cuda_runtime.h>
+#include <math.h>
+
+__global__ void tanh_kernel(const float* input, float* output, int N) {
+    // Write code here
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (i >= N){
+        return;
+    }
+
+    output[i] = (expf(input[i]) - expf(-input[i])) / (expf(input[i]) + expf(-input[i]));
+    return;
+}
+
+extern "C" void solve(const float* input, float* output, int N) {
+    int threads = 256;
+    int blocks = (N + threads - 1) / threads;
+    tanh_kernel<<<blocks, threads>>>(input, output, N);
+    cudaDeviceSynchronize();
+}
