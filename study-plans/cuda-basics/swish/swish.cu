7709fae1b06b9a4d2bfafecf3579b38da8730b33
Raghav K <ragg04@gmail.com>
2026-08-22 21:50:35 -0700

    Solve swish

---
+++ study-plans/cuda-basics/swish/swish.cu
@@ -0,0 +1,20 @@
+#include <cuda_runtime.h>
+#include <math.h>
+
+__global__ void swish_kernel(const float* input, float* output, int N) {
+    // Write code here
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (i >= N){
+        return;
+    }
+    output[i] = input[i] / (1 + expf(-input[i]));
+    return;
+}
+
+extern "C" void solve(const float* input, float* output, int N) {
+    int threads = 256;
+    int blocks = (N + threads - 1) / threads;
+    swish_kernel<<<blocks, threads>>>(input, output, N);
+    cudaDeviceSynchronize();
+}
