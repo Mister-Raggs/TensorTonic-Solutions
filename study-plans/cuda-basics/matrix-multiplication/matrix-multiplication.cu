e5c65c31113dc9cfa2b36bcf24dca8d0183efba3
Raghav K <ragg04@gmail.com>
2026-08-23 23:16:48 -0700

    Solve matrix-multiplication

---
+++ study-plans/cuda-basics/matrix-multiplication/matrix-multiplication.cu
@@ -0,0 +1,24 @@
+#include <cuda_runtime.h>
+
+__global__ void matmul_kernel(const float* A, const float* B, float* C, int M, int N, int K) {
+    // Write code here
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (col >= N or row >= M){
+        return;
+    }
+
+    C[row*N + col] = 0;
+    for (int k = 0; k < K; k++){
+        C[row*N + col] += A[row*K + k] * B[k*N + col]; 
+    }
+    return;
+}
+
+extern "C" void solve(const float* A, const float* B, float* C, int M, int N, int K) {
+    dim3 threads(16, 16);
+    dim3 blocks((N + 15) / 16, (M + 15) / 16);
+    matmul_kernel<<<blocks, threads>>>(A, B, C, M, N, K);
+    cudaDeviceSynchronize();
+}
