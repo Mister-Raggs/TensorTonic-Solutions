a4c44c4a0b29c86e03274f6dea1a2ff085cf3bbf
Raghav K <ragg04@gmail.com>
2026-08-24 14:37:43 -0700

    Solve hadamard-product

---
+++ study-plans/cuda-basics/hadamard-product/hadamard-product.cu
@@ -0,0 +1,20 @@
+#include <cuda_runtime.h>
+
+__global__ void hadamard_kernel(const float* A, const float* B, float* C, int M, int N) {
+    // Write code here
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (col >= N or row >= M){
+        return;
+    }
+    C[row*N + col] = A[row*N + col] * B[row*N + col];
+    return;
+}
+
+extern "C" void solve(const float* A, const float* B, float* C, int M, int N) {
+    dim3 threads(16, 16);
+    dim3 blocks((N + 15) / 16, (M + 15) / 16);
+    hadamard_kernel<<<blocks, threads>>>(A, B, C, M, N);
+    cudaDeviceSynchronize();
+}
