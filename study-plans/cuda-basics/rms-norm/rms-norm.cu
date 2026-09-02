d88a95e060d20c58d1d9dac051088ff8ba2ec09f
Raghav K <ragg04@gmail.com>
2026-09-01 22:54:51 -0700

    Solve rms-norm

---
+++ study-plans/cuda-basics/rms-norm/rms-norm.cu
@@ -0,0 +1,30 @@
+#include <cuda_runtime.h>
+#include <math.h>
+
+__global__ void rms_norm_kernel(const float* input, const float* gamma, float* output, int M, int N, float eps) {
+    // Write code here
+    int row = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (row >= M){
+        return;
+    }
+    float rms = 0.0;
+
+    for (int i = 0; i < N; i++){
+        rms += input[row*N + i] * input[row*N + i];
+    }
+    rms = sqrtf((rms / N) + eps);
+
+    for (int j = 0; j < N; j++){
+        output[row*N + j] = input[row*N + j] * gamma[j] / rms; 
+    }
+    return;
+    
+}
+
+extern "C" void solve(const float* input, const float* gamma, float* output, int M, int N, float eps) {
+    int threads = 256;
+    dim3 blocks(M);
+    rms_norm_kernel<<<blocks, threads>>>(input, gamma, output, M, N, eps);
+    cudaDeviceSynchronize();
+}
